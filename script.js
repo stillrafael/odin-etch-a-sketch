f198555f51b9395bfb01baefe01b941908f7d77b
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-29 18:24:15 -0300

    Draws a webpage and change elements
    still needs to accept user input to create squares, restart without having to refresh, etc

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const gridSpace = document.querySelector('div')
 gridSpace.textContent = ''
 gridSpace.classList.add('container')
-for (i = 0; i < 4; i++) {
+for (i = 0; i < 16; i++) {
     const gridSquare = document.createElement('div')
     gridSquare.className = 'grid'
     gridSpace.appendChild(gridSquare)
+    gridSquare.addEventListener('mouseover', swappingColors)
+    function swappingColors () {
+        gridSquare.classList.add('white')
+    }
 }
+
+
